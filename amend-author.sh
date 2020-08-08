a8125dd33c7e50f4365f32271d409d4d251079d6
RequireSun <kelvinlpsun@hotmail.com>
2020-08-08 20:08:35 +0800

    chore: 改名脚本

--- amend-author.sh
+++ amend-author.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 git filter-branch --env-filter '
-OLD_EMAIL="OLD_EMAIL"
-CORRECT_NAME="NEW_NAME"
-CORRECT_EMAIL="NEW_EMAIL"
+OLD_EMAIL="requiresun@L-65H6JG5H-1304.local"
+CORRECT_NAME="RequireSun"
+CORRECT_EMAIL="kelvinlpsun@hotmail.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-    export GIT_COMMITTER_NAME="RequireSun"
-    export GIT_COMMITTER_EMAIL="kelvinlpsun@hotmail.com"
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
 if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-    export GIT_AUTHOR_NAME="RequireSun"
-    export GIT_AUTHOR_EMAIL="kelvinlpsun@hotmail.com"
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
